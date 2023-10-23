21e62077813b22881af88a28b9e6fdd0c4ca7a01
Masud Shafin Ahmed <msaoct@MacBook-Pro.local>
2023-10-24 01:46:18 +0530

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import MyReel from "@/components/MyReel";
+import InstagramReelSearch from "@/components/Reel";
 
 export default function Home() {
   return (
     <div className='w-full m-auto flex-col items-center justify-center my-4'>
     <h1 className=' text-center'>Instagram Reel Downloader</h1>
-    <MyReel/>
+    {/* <MyReel/> */}
+    <InstagramReelSearch/>
   </div>
   )
 }
