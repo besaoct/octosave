7931314d38ee5dd7bf3d035a724f372284b2522e
Masud Shafin Ahmed <msaoct@MacBook-Pro.local>
2023-10-23 15:15:42 +0530

    init

--- next.config.js
+++ next.config.js
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'scontent.cdninstagram.com',
-              port: "",
-              pathname: "**",
-      
-            }
-        ]
-      },
-}
 
-module.exports = nextConfig
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+module.exports = withPWA({
+   images: {
+    domains: ["misc.scdn.co", "i.scdn.co", "geo-media.beatsource.com", "i1.sndcdn.com", "media.pitchfork.com", "seed-mix-image.spotifycdn.com", "xrxqqakkgezhaqsfgjey.supabase.co"]
+  }
+})
