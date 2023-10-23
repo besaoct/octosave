cbebe0bdfab7542897471d063a268ef08e5727aa
Masud Shafin Ahmed <msaoct@MacBook-Pro.local>
2023-10-24 01:35:15 +0530

    init

--- app/api/getReel/route.ts
+++ app/api/getReel/route.ts
@@ -1,14 +1,16 @@
 // import axios from 'axios';
 import { NextRequest, NextResponse } from 'next/server';
 import puppeteer from 'puppeteer';
-
+export const runtime = 'edge';
 export async function POST(request: NextRequest) {
   try {
     const req = await request.json();
     const { id } = req;
     const url = `https://www.instagram.com/reel/${id}`;
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      headless: "new"
+    });
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: 'domcontentloaded' });
 
